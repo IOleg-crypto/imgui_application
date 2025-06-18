1847baff0fd0875c4b207a8c35fd5de2e99292fa
IOleg-sus <guchkooleg7@gmail.com>
2025-06-18 12:11:25 +0300

    code : refactoring and make cleaner

---
+++ MyAppImGUI/src/Window/CWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 38063 on 18.06.2025.
+//
+
+#include "CWindow.h"
---
+++ MyAppImGUI/src/Window/CWindow.h
@@ -0,0 +1,16 @@
+//
+// Created by 38063 on 18.06.2025.
+//
+
+#ifndef CWINDOW_H
+#define CWINDOW_H
+
+
+
+class CWindow {
+
+};
+
+
+
+#endif //CWINDOW_H
