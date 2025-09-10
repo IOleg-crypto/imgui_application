2415e6d87fb3786be3f029783935cdce9ed30891
I#Oleg <guchkooleg7@gmail.com>
2025-09-10 09:31:04 -0400

    update imgui and .gitmodules

--- MyAppImGUI/build/_deps/stb-subbuild/cmake_install.cmake
+++ MyAppImGUI/build/_deps/stb-subbuild/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/gitnext/CppWithImGUI/imgui_application/MyAppImGUI/build/_deps/stb-subbuild
+# Install script for directory: /home/kali/linux_project/imgui_application/MyAppImGUI/build/_deps/stb-subbuild
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/stb-populate")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -35,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/gitnext/CppWithImGUI/imgui_application/MyAppImGUI/build/_deps/stb-subbuild/install_local_manifest.txt"
+  file(WRITE "/home/kali/linux_project/imgui_application/MyAppImGUI/build/_deps/stb-subbuild/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/gitnext/CppWithImGUI/imgui_application/MyAppImGUI/build/_deps/stb-subbuild/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/kali/linux_project/imgui_application/MyAppImGUI/build/_deps/stb-subbuild/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
